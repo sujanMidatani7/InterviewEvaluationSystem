ceacfc198081ec31832d70f59aa6979a552115fe
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-06-25 22:19:54 +0530

    Update main.py

--- Huggingface Spaces/Resume Details to Questions Generation/main.py
+++ Huggingface Spaces/Resume Details to Questions Generation/main.py
@@ -5,12 +5,12 @@ from langchain.prompts.prompt import PromptTemplate
 
 load_dotenv() 
 
-def generate_questions(resume, role='', experience=''):
+def generate_questions(resume_details, role='', experience=''):
     """
     Generates interview questions based on the given resume, desired role, and experience.
 
     Args:
-        resume (str): The user's resume details.
+        resume_details (str): The user's resume details.
         role (str, optional): The role the user wants to join. Defaults to ''.
         experience (str, optional): The user's relevant experience. Defaults to ''.
 
@@ -32,7 +32,7 @@ def generate_questions(resume, role='', experience=''):
     llm1 = OpenAI(model_name="text-davinci-003", temperature=0)
     chain = LLMChain(llm=llm1, prompt=PROMPT)
     prompt = chain.predict_and_parse(
-        resume_details=resume,
+        resume_details=resume_details,
         role=role,
         experience=experience,
         questions=10
